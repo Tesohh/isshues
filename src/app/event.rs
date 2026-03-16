e984144371ca072cbd3b5796c177a8656f4b2e2c
Simone Tesini <simozilla006@gmail.com>
2026-03-16 13:50:31 +0100

    remove all rust

--- src/app/event.rs
+++
@@ -1,9 +0,0 @@
-/// Events sent from the server, or the handler, to the clients
-#[derive(Clone)]
-pub enum ToClientEvent {
-    Input(termwiz::input::InputEvent),
-}
-
-/// Events sent from the clients to the server
-#[derive(Clone)]
-pub enum ToServerEvent {}
