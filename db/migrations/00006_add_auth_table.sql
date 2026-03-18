6823c87c19e862ef39d3edcc64c63ec57ba1af2f
Simone Tesini <simozilla006@gmail.com>
2026-03-18 23:53:46 +0100

    rename ssh table

--- db/migrations/00006_add_auth_table.sql
+++ db/migrations/00006_add_auth_table.sql
@@ -1,8 +1,8 @@
 -- +goose Up
-CREATE TABLE users_ssh_pks (
+CREATE TABLE user_ssh_keys (
     user_id BIGINT NOT NULL REFERENCES users(id),
     ssh_public_key text NOT NULL
 );
 
 -- +goose Down
-DROP TABLE users_ssh_pks;
+DROP TABLE user_ssh_keys;
