a98b75d134bf26e5e6a23a691f5f98dfb7ca0720
Simone Tesini <simozilla006@gmail.com>
2026-03-23 18:27:08 +0100

    use lipgloss.Color instead of hex function

--- config/make_logo.go
+++ config/make_logo.go
@@ -4,23 +4,12 @@ import (
 	"fmt"
 
 	"charm.land/lipgloss/v2"
-	"charm.land/log/v2"
-	"github.com/Tesohh/isshues/common"
 	"github.com/spf13/viper"
 )
 
 func MakeLogo(viper *viper.Viper) string {
-	bg, err := common.HexToLipgloss(viper.GetString("company.logo_bg"))
-	if err != nil {
-		log.Warn("company.logo_bg hex parse error", "err", err)
-		return "ERROR"
-	}
-
-	fg, err := common.HexToLipgloss(viper.GetString("company.logo_fg"))
-	if err != nil {
-		log.Warn("company.logo_fg hex parse error", "err", err)
-		return "ERROR"
-	}
+	bg := lipgloss.Color(viper.GetString("company.logo_bg"))
+	fg := lipgloss.Color(viper.GetString("company.logo_fg"))
 
 	style := lipgloss.NewStyle().Background(bg).Foreground(fg).PaddingLeft(1).PaddingRight(2) // kabashi
 	logo := style.Render(viper.GetString("company.logo"))
