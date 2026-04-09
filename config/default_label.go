db6d9f7264ea68bfcb85b5555f6aa5bd3306bfbb
Simone Tesini <simozilla006@gmail.com>
2026-04-10 00:09:53 +0200

    remove comments in default label

--- config/default_label.go
+++ config/default_label.go
@@ -5,22 +5,3 @@ type DefaultLabel struct {
 	Color  string `mapstructure:"color"`
 	Symbol string `mapstructure:"symbol"`
 }
-
-// # Labels that are added by default on project creation. Recommended to keep some "well defined" labels for common categories.
-// [[default_labels]]
-// # anything that improves the project: new features, optimizations, refactorings...
-// name = "feat"
-// color = "blue"
-// symbol = "󰇈"
-//
-// [[default_labels]]
-// # unexpected bugs or behaviours that pop up and need to be fixed. goes well with !crit...
-// name = "fix"
-// color = "red"
-// symbol = ""
-//
-// [[default_labels]]
-// # anything that is required to do, but doesn't "touch" the code, eg. documentation, presentations, submissions, deadlines, advertisements...
-// name = "chore"
-// color = "green"
-// symbol = "󰃢"
