21ff5bb680ad784061c1edc0de7184598d79515c
Simone Tesini <simozilla006@gmail.com>
2026-05-03 02:28:36 +0200

    split up NavModel interface

--- model/navmodel.go
+++ model/navmodel.go
@@ -2,18 +2,31 @@ package model
 
 import tea "charm.land/bubbletea/v2"
 
-type NavModel interface {
+// SubModel describes models that are controlled by the Root model or other SubModels.
+type SubModel[M SubModel[M]] interface {
 	Init() tea.Cmd
-	Update(tea.Msg) (NavModel, tea.Cmd)
+	Update(tea.Msg) (M, tea.Cmd)
 	View() string
+}
 
-	// This model shall provide help keys
-	Helper
-
-	// Title used for statusbar, window title...
+// Titler describes any model that can provide a title
+// (eg. used for showing in the statusbar, window title...)
+type Titler interface {
 	Title() string
+}
 
-	// similar to Init(), called when the model becomes active but already exists.
-	// for example, when receiving a Refresh message, set a "dirty" bit, and on Rehydrate, query new data if "dirty"
+// Rehydrater describes any model that can be rehydrated,
+// similarly to Init(), called when the model becomes active but already exists.
+// for example, when receiving a Refresh message, set a "dirty" bit, and on Rehydrate, query new data if "dirty"
+type Rehydrater interface {
 	Rehydrate() tea.Cmd
+}
+
+// NavModel describes any model that can be placed in the NavStack of the root model.
+// It is a submodel and it also must be able to provide Help keys, a title, and be able to be rehydrated
+type NavModel interface {
+	SubModel[NavModel]
+	Helper
+	Titler
+	Rehydrater
 }
